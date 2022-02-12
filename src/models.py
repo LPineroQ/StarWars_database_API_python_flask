067c27faf19639910fc957654c53717f976d6378
Luis Piñero Quintana <lpineroq@gmail.com>
2022-02-12 12:02:46 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -17,3 +17,62 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Models):
+    
+        __tablename__= 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(500))
+    url = db.Column(db.String(200))
+    surface_water = db.Column(db.Integer)
+    terrain = db.Column(db.String(120))
+    climate = db.Column(db.String(120))
+    population = db.Column(db.Integer)
+    gravity = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+
+class People(Base):
+    __tablename__= 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(300))
+    url = db.Column(db.String(120))
+    homeworld = db.Column(db.String(120))
+    gender = db.Column(db.String(50))
+    birth_year = db.Column(db.Integer)
+    eye_color = db.Column(db.String(120))
+    skin_color = db.Column(db.String(120))
+    hair_color = db.Column(db.String(120))
+    mass = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+
+class Vehicles(Base):
+    __tablename__= 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(300))
+    url = db.Column(db.String(120))
+    model = db.Column(db.String(120))
+    starship_class = db.Column(db.String(120))
+    manufacturer = db.Column(db.String(120))
+    cost_in_credits = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    max_atmosphering_speed = db.Column(db.Integer)
+    hyperdrive_rating = db.Column(db.Integer)
+    MGLT = db.Column(db.Integer)
+    cargo_capacities = db.Column(db.Integer)
+    consumables = db.Column(db.Integer)
+    pilots = db.Column(db.String(120))
+
+class favorites(Base):
+    __tablename__= 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=True)
+    Planet_id= db.Column(db.Integer, ForeignKey('planets.id'), nullable=True)
+    People_id= db.Columndb.(Integer, ForeignKey('people.id'), nullable=True)
+    Vehicles_id= db.Column(db.Integer, ForeignKey('vehicles.id'), nullable=True)
